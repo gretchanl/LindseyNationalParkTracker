09704e8c7527065d5ffd969ef8f6db2565c7ade6
G Lindsey <gretchanl@hotmail.com>
2016-04-13 19:28:03 -0700

    trying to return a ParkName!

--- NatlParkTracker/Park.cs
+++ NatlParkTracker/Park.cs
@@ -6,21 +6,39 @@ using System.Threading.Tasks;
 
 namespace NatlParkTracker
 {
+
+    public enum ParkRegion
+    {
+        Alaska,
+        Northeast,
+        InterMountain,
+        Midwest,
+        NationalCapital,
+        PacificWest, 
+        Southeast
+    }
+    
     /// <summary>
     /// This is data for each park from the Park Service
     /// </summary>
-    public class Park
+    public class MyVisit
     {
         #region Properties
         
-        public string Name { get; set; }
-        public string Abbreviation { get; set; }
-        public string Region { get; set; }
+        public string ParkName { get; set; }
+        public string Abbreviation { get; set; } // like NP, NPL
+        // public string Region { get; set; } - created an enum for this
         public string State { get; set; }
+        public ParkRegion Region { get; set; }
+
         #endregion
-       
+
         #region Methods
-      
+
+        public string MyVisit(string ParkName)
+        {
+            return visit1;
+        }
 
         #endregion
     }
--- NatlParkTracker/Program.cs
+++ NatlParkTracker/Program.cs
@@ -10,6 +10,16 @@ namespace NatlParkTracker
     {
         static void Main(string[] args)
         {
+            //creating an instance of a visit
+            var visit1 = new MyVisit(); // allocates memory
+
+            //visit1.Visit = "This Visit";
+            visit1.Region = ParkRegion.PacificWest;
+            visit1.ParkName = ("Yellowstone");
+            Console.WriteLine("We visited: {0}",
+                visit1.ParkName);
+            
+
         }
     }
 }
