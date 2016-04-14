b3055a52fdf649eb5f35f8418a567b920979373f
G Lindsey <gretchanl@hotmail.com>
2016-04-13 17:46:19 -0700

    new code

---
+++ NatlParkTracker/Park.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatlParkTracker
+{
+    /// <summary>
+    /// This is data for each park
+    /// </summary>
+    public class Park
+    {
+        #region Properties
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public string Region { get; set; }
+        public string State { get; set; }
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+    }
+}
+        
